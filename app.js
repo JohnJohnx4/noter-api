6a2a75d3e1939cf8dc95514c159d8e7cc66a8327
John <johnjohnx4@gmail.com>
2019-05-09 23:16:45 -0400

    added useNerUrlParser

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/routes');
+const PORT = process.env.PORT || 5000;
+const app = express();
+const path = require('path');
+const rl = require('readline');
+
+const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/noter';
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    return console.log('Connected to mongo database');
+  })
+  .catch(err => console.log('error with MongoDB', err));
+
+routes(app);
+
+app.listen(PORT, err => {
+  if (err) console.log(err);
+  console.log('app listening on: ', PORT);
+});
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+
+app.options('*', cors({ origin: '*' }));
+app.get('/', (req, res) => {
+  res.status(200).json('Hey, this is my server!');
+});
